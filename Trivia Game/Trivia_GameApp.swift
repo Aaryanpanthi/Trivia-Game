3b54e66058f7d0e3b6e7192a13ed29dc3e9a2d32
Aaryan1604 <aryanpanthi3@gmail.com>
2026-03-09 22:14:25 -0400

    Initial Commit

---
+++ Trivia Game/Trivia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameApp.swift
+//  Trivia Game
+//
+//  Created by Aaryan Panthi on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
